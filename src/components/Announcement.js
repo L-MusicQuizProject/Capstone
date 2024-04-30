d09232068e292b3a5c98925a8b63aab3eb803208
Adrian Guerrero <adrianguerrero@lewisu.edu>
2024-04-30 11:08:43 -0500

    Changes

--- src/components/Announcement.js
+++
@@ -1,8 +0,0 @@
-
-import classes from '../styles/Announcement.module.css';
-
-const Announcement = () => {
-  return <div className={classes.announcement}>"Music can change the world because it can change people"-Bono.</div>;
-};
-
-export default Announcement;
